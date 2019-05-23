38b31650eb3770f99e112db80d379534de4021ba
Alexander Schelchere <ascellie@gmail.com>
2019-05-23 09:26:28 -0400

    keys of hash - complete

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
+# Now, we know what our hash looks like:
+# {"sugar glider"=>"Australia", "aye-aye"=>"Madagascar", "red-footed tortoise"=>"Panama", "kangaroo"=>"Australia", "tomato frog"=>"Madagascar", "koala"=>"Australia"}
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*animals)
+
+    # array to store the keys of the arguments
+    keys = []
+
+    # map through the instance of the hash
+    self.map do |animal, country| 
+
+      if animals.include?(country) 
+        keys << animal
+      end
+
+    end
+    
+    keys
   end
 end
